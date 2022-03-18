db374b81ef89c90314205fa550a39ae252e7384e
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-18 14:01:42 -0700

    View Image Screen

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button, Alert, Dimensions } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
 
   const { landscape } = useDeviceOrientation();
 
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
