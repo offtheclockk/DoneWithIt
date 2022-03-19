ce15f8c97b7dc274472474e9a2017912aca06ed5
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-19 13:06:49 -0700

    welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+import AppButton from '../components/AppButton';
 
 function WelcomeScreen(props) {
           return (
                     <ImageBackground
+                              blurRadius={10}
                               style={styles.background}
                               source={require('../assets/background.jpg')}>
                               <View
@@ -11,13 +13,11 @@ function WelcomeScreen(props) {
                                         <Image
                                                   style={styles.logo}
                                                   source={require('../assets/logo-red.png')} />
-                                        <Text>Sell What You Don't Need</Text>
+                                        <Text style={styles.tagline}>Sell What You Don't Need</Text>
                               </View>
-                              <View
-                                        style={styles.loginButton}>
-                              </View>
-                              <View
-                                        style={styles.registerButton}>
+                              <View style={styles.buttonsContainer}>
+                                        <AppButton title="Login" />
+                                        <AppButton title="Register" color="secondary" />
                               </View>
                     </ImageBackground>
           );
@@ -29,15 +29,9 @@ const styles = StyleSheet.create({
                     justifyContent: "flex-end",
                     alignItems: "center"
           },
-          loginButton: {
+          buttonsContainer: {
+                    padding: 20,
                     width: "100%",
-                    height: 70,
-                    backgroundColor: '#fc5c65',
-          },
-          registerButton: {
-                    width: "100%",
-                    height: 70,
-                    backgroundColor: '#4ecdc4',
           },
           logo: {
                     width: 100,
@@ -47,6 +41,11 @@ const styles = StyleSheet.create({
                     position: "absolute",
                     top: 70,
                     alignItems: "center"
+          },
+          tagline: {
+                    fontSize: 25,
+                    fontWeight: "600",
+                    paddingVertical: 20
           }
 })
 
