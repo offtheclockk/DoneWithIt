751d7fd0042ad23e5d15fec77974c7527da4b791
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-20 13:41:49 -0700

    screen changes

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 import ListItem from '../components/ListItem';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
-const messages = [
+const initialMessages = [
           {
                     id: 1,
                     title: 'T1',
@@ -21,6 +22,14 @@ const messages = [
 ]
 
 function MessagesScreen(props) {
+          const [messages, setMessages] = useState(initialMessages);
+          const [refreshing, setRefreshing] = useState(false);
+
+          const handleDelete = message => {
+                    //Delete the message from messages
+                    setMessages(messages.filter(m => m.id !== message.id));
+          }
+
           return (
                     <Screen style={styles.screen}>
                               <FlatList
@@ -31,8 +40,23 @@ function MessagesScreen(props) {
                                                             title={item.title}
                                                             subTitle={item.description}
                                                             image={item.image}
+                                                            onPress={() => console.log("Message selected", item)}
+                                                            renderRightActions={() =>
+                                                                      <ListItemDeleteAction onPress={() => handleDelete(item)}
+                                                                      />}
                                                   />}
-                                        ItemSeparatorComponent={ListItemSeparator} />
+                                        ItemSeparatorComponent={ListItemSeparator}
+                                        refreshing={refreshing}
+                                        onRefresh={() => {
+                                                  setMessages([
+                                                            {
+                                                                      id: 2,
+                                                                      title: 'T2',
+                                                                      description: 'D2',
+                                                                      image: require('../assets/tristan.jpg')
+                                                            },
+                                                  ])
+                                        }} />
                     </Screen>
           );
 }
