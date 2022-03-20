cd36559d7cf6ba75cacadcc2117b2c642533f8a1
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-20 12:38:24 -0700

    Screen updates

--- App.js
+++ App.js
@@ -7,13 +7,14 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import AppButton from './app/components/AppButton';
 import AppText from './app/components/AppText';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
 
   const { landscape } = useDeviceOrientation();
 
   return (
-    <ListingDetailsScreen />
+    <MessagesScreen />
   );
 }
 
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import AppText from '../components/AppText';
+import ListItem from '../components/ListItem';
 
 import colors from '../config/colors';
 
@@ -11,6 +12,13 @@ function ListingDetailsScreen(props) {
                               <View style={styles.detailsContainer}>
                                         <AppText style={styles.title}>Red jacket for sale</AppText>
                                         <AppText style={styles.price}>$100</AppText>
+                                        <View style={styles.userContainer}>
+                                                  <ListItem
+                                                            image={require("../assets/tristan.jpg")}
+                                                            title="Tristan White"
+                                                            subTitle="5 Listings"
+                                                  />
+                                        </View>
                               </View>
                     </View>
           );
@@ -33,7 +41,10 @@ const styles = StyleSheet.create({
           title: {
                     fontSize: 24,
                     fontWeight: "500"
-          }
+          },
+          userContainer: {
+                    marginVertical: 40,
+          },
 })
 
 export default ListingDetailsScreen;
