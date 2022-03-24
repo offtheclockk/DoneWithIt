5ed6f9365f3a7aacb25b7df76812ed8bb7c4fb2b
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-24 11:56:37 -0700

    app picker

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button, Alert, Dimensions, TextInput } from 'react-native';
+import React, { useState } from 'react'
+  ; import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button, Alert, Dimensions, TextInput, Switch } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -14,14 +15,16 @@ import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
 
 export default function App() {
 
-  const { landscape } = useDeviceOrientation();
+  const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email" />
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
