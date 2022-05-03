3b03c3e9279fa0545e1b0f17bc7fe31accaf7ad7
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-05-02 17:21:30 -0700

    listing edit screen

--- App.js
+++ App.js
@@ -1,37 +1,11 @@
-import React, { useState } from 'react'
-  ; import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button, Alert, Dimensions, TextInput, Switch } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import AppButton from './app/components/AppButton';
-import AppText from './app/components/AppText';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-];
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
 
-  const [category, setCategory] = useState(categories[0]);
-
   return (
-    <Screen>
-      <AppPicker selectedItem={category} onSelectItem={item => setCategory(item)} items={categories} icon="apps" placeholder="Category" />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
+    <ListingEditScreen />
   );
 }
 
