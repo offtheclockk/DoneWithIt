f44d7f2dff91ad723d00789c655bbbf41f0ba77e
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-05-02 17:21:47 -0700

    screens

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { StyleSheet, Image } from 'react-native';
+import * as Yup from 'yup';
+
+import Screen from '../components/Screen';
+import { AppForm, AppFormField, SubmitButton } from '../components/forms'
+
+const validationSchema = Yup.object().shape({
+          name: Yup.string().required().email().label("Name"),
+          email: Yup.string().required().email().label("Email"),
+          password: Yup.string().required().min(4).matches().label("Password")
+})
+
+function RegisterScreen(props) {
+
+          return (
+                    <Screen style={styles.container}>
+                              <Image
+                                        style={styles.logo}
+                                        source={require("../assets/logo-red.png")}
+                              />
+                              <AppForm
+                                        initialValues={{ name: '', email: '', password: '' }}
+                                        onSubmit={values => console.log(values)}
+                                        validationSchema={validationSchema}
+                              >
+                                        <AppFormField
+                                                  autoCapitalize="none"
+                                                  autoCorrect={false}
+                                                  icon="account"
+                                                  keyboardType="email-address"
+                                                  name="name"
+                                                  placeholder="Name"
+                                                  textContentType="name"
+                                        />
+                                        <AppFormField
+                                                  autoCapitalize="none"
+                                                  autoCorrect={false}
+                                                  icon="email"
+                                                  keyboardType="email-address"
+                                                  name="email"
+                                                  placeholder="Email"
+                                                  textContentType="emailAddress"
+                                        />
+                                        <AppFormField
+                                                  autoCapitalize="none"
+                                                  autoCorrect={false}
+                                                  icon="lock"
+                                                  name="password"
+                                                  placeholder="Password"
+                                                  secureTextEntry
+                                                  textContentType="password"
+                                        />
+                                        <SubmitButton title="Register" />
+                              </AppForm>
+                    </Screen>
+          );
+}
+
+const styles = StyleSheet.create({
+          container: {
+                    padding: 10
+          },
+
+          logo: {
+                    width: 80,
+                    height: 80,
+                    alignSelf: 'center',
+                    marginTop: 50,
+                    marginBottom: 20
+          },
+})
+
+export default RegisterScreen;
