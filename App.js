530b76540db5c75a6b38962b5b1482a5ca6ceac7
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-05-10 12:11:29 -0700

    testing permissions

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import MessagesScreen from './app/screens/MessagesScreen'
 import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted)
+      alert('You need to enable permission to access the library')
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled)
+        setImageUri(result.uri);
+    } catch (error) {
+      console.log('Error reading an image')
+    }
+  }
 
   return (
-    <ListingEditScreen />
+    <Screen>
+      <Button title="Select image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
   );
 }
 
