5b5ec3a4ac82a2b53b4a84026533134456ab4a1a
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-05-17 11:00:56 -0700

    styling changes

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -3,13 +3,21 @@ import { View, TextInput, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import defaultStyles from '../config/styles';
-import { Platform } from 'react-native-web';
 
 function AppTextInput({ icon, width = "100%", ...otherProps }) {
           return (
                     <View style={[styles.container, { width }]} >
-                              {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
-                              <TextInput style={defaultStyles.text} {...otherProps} />
+                              {icon && <MaterialCommunityIcons
+                                        name={icon}
+                                        size={20}
+                                        color={defaultStyles.colors.medium}
+                                        style={styles.icon}
+                              />}
+                              <TextInput
+                                        placeholderTextColor={defaultStyles.colors.medium}
+                                        style={defaultStyles.text}
+                                        {...otherProps}
+                              />
                     </View>
           );
 }
--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import AppText from './AppText';
-import Icon from './Icon';
+import React from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+
+import Icon from "./Icon";
+import AppText from "./AppText";
 
 function CategoryPickerItem({ item, onPress }) {
-          return <View style={styles.container}>
-                    <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
-                    <AppText style={styles.label}>{item.label}</AppText>
-          </View>
+          return (
+                    <View style={styles.container}>
+                              <TouchableOpacity onPress={onPress}>
+                                        <Icon
+                                                  backgroundColor={item.backgroundColor}
+                                                  name={item.icon}
+                                                  size={80}
+                                        />
+                              </TouchableOpacity>
+                              <AppText style={styles.label}>{item.label}</AppText>
+                    </View>
+          );
 }
 
 const styles = StyleSheet.create({
           container: {
-                    paddingHorizontal: 20,
+                    paddingHorizontal: 30,
                     paddingVertical: 15,
-                    alignItems: 'center',
+                    alignItems: "center",
                     width: "33%",
           },
           label: {
                     marginTop: 5,
-                    textAlign: 'center'
-          }
+                    textAlign: "center",
+                    fontSize: 15,
+          },
 });
 
 export default CategoryPickerItem;
