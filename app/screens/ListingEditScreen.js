07f097db0c4b2561936f82e605c1c1b283f5d8cb
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-05-17 11:01:08 -0700

    upload photos

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -8,22 +8,77 @@ import AppFormField from '../components/forms/AppFormField';
 import SubmitButton from '../components/forms/SubmitButton';
 import Screen from "../components/Screen";
 import CategoryPickerItem from '../components/CategoryPickerItem';
+import FormImagePicker from '../components/forms/FormImagePicker';
+import useLocation from '../hooks/useLocation';
 
 const validationSchema = Yup.object().shape({
           title: Yup.string().required().min(1).label("Title"),
           price: Yup.number().required().min(1).max(10000).label("Price"),
           description: Yup.string().label("Description"),
-          category: Yup.object().required().nullable().label("Category")
+          category: Yup.object().required().nullable().label("Category"),
+          images: Yup.array().min(1, "Please select at least one image.")
 });
 
 const categories = [
-          { label: "Furniture", value: 1, backgroundColor: 'red', icon: 'apps' },
-          { label: "Clothing", value: 2, backgroundColor: 'green', icon: 'email' },
-          { label: "Camera", value: 3, backgroundColor: 'blue', icon: 'lock' },
-
+          {
+                    backgroundColor: "#fc5c65",
+                    icon: "floor-lamp",
+                    label: "Furniture",
+                    value: 1,
+          },
+          {
+                    backgroundColor: "#fd9644",
+                    icon: "car",
+                    label: "Cars",
+                    value: 2,
+          },
+          {
+                    backgroundColor: "#fed330",
+                    icon: "camera",
+                    label: "Cameras",
+                    value: 3,
+          },
+          {
+                    backgroundColor: "#26de81",
+                    icon: "cards",
+                    label: "Games",
+                    value: 4,
+          },
+          {
+                    backgroundColor: "#2bcbba",
+                    icon: "shoe-heel",
+                    label: "Clothing",
+                    value: 5,
+          },
+          {
+                    backgroundColor: "#45aaf2",
+                    icon: "basketball",
+                    label: "Sports",
+                    value: 6,
+          },
+          {
+                    backgroundColor: "#4b7bec",
+                    icon: "headphones",
+                    label: "Movies & Music",
+                    value: 7,
+          },
+          {
+                    backgroundColor: "#a55eea",
+                    icon: "book-open-variant",
+                    label: "Books",
+                    value: 8,
+          },
+          {
+                    backgroundColor: "#778ca3",
+                    icon: "application",
+                    label: "Other",
+                    value: 9,
+          },
 ];
 
-function ListingEditScreen(props) {
+function ListingEditScreen() {
+          const location = useLocation();
+
           return (
                     <Screen style={styles.container}>
                               <AppForm
@@ -32,11 +87,17 @@ function ListingEditScreen(props) {
                                                   price: '',
                                                   description: '',
                                                   category: null,
+                                                  images: [],
                                         }}
-                                        onSubmit={(values) => console.log(values)}
+                                        onSubmit={(values) => console.log(location)}
                                         validationSchema={validationSchema}
                               >
-                                        <AppFormField maxLength={255} name="title" placeholder="Title" />
+                                        <FormImagePicker name="images" />
+                                        <AppFormField
+                                                  maxLength={255}
+                                                  name="title"
+                                                  placeholder="Title"
+                                        />
                                         <AppFormField
                                                   keyboardType="numeric"
                                                   maxLength={8}
