3825b84564f96731328bb84f02da27a1e65fba39
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-06-23 13:01:49 -0700

    upload screen and animation

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 
+import listings from '../api/listings';
 import AppFormPicker from '../components/forms/AppFormPicker';
 import AppForm from '../components/forms/AppForm';
 import AppFormField from '../components/forms/AppFormField';
@@ -10,6 +11,7 @@ import Screen from "../components/Screen";
 import CategoryPickerItem from '../components/CategoryPickerItem';
 import FormImagePicker from '../components/forms/FormImagePicker';
 import useLocation from '../hooks/useLocation';
+import UploadScreen from './UploadScreen';
 
 const validationSchema = Yup.object().shape({
           title: Yup.string().required().min(1).label("Title"),
@@ -79,8 +81,27 @@ const categories = [
 function ListingEditScreen() {
           const location = useLocation();
 
+          const [uploadVisible, setUploadVisible] = useState(false);
+          const [progress, setProgress] = useState(0);
+
+          const handleSubmit = async (listing, { resetForm }) => {
+                    setProgress(0);
+                    setUploadVisible(true);
+                    const result = await listings.addListing(
+                              { ...listing, location },
+                              (progress) => setProgress(progress)
+                    );
+
+                    if (!result.ok) {
+                              setUploadVisible(false);
+                              return alert('Could not save the listing.');
+                    }
+                    resetForm();
+          }
+
           return (
                     <Screen style={styles.container}>
+                              <UploadScreen onDone={() => setUploadVisible(false)} progress={progress} visible={uploadVisible} />
                               <AppForm
                                         initialValues={{
                                                   title: '',
@@ -89,7 +110,7 @@ function ListingEditScreen() {
                                                   category: null,
                                                   images: [],
                                         }}
-                                        onSubmit={(values) => console.log(location)}
+                                        onSubmit={handleSubmit}
                                         validationSchema={validationSchema}
                               >
                                         <FormImagePicker name="images" />
