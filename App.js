5f4fad93d5b63fc78c86f4340e192eb16b81ba84
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-06-27 17:11:18 -0700

    store user

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Text, Button } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import NetInfo from '@react-native-community/netinfo';
+import AppLoading from 'expo-app-loading';
 
 import Screen from './app/components/Screen';
 import AuthNavigator from './app/navigation/AuthNavigation';
@@ -12,6 +13,7 @@ import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
 import AuthContext from './app/auth/context';
+import storage from './app/auth/storage';
 
 const Link = () => {
       const navigation = useNavigation();
@@ -83,7 +85,17 @@ const TabNavigator = () => (
 
 export default function App() {
       const [user, setUser] = useState();
+      const [isReady, setIsReady] = useState(false);
 
+      const restoreUser = async () => {
+            const user = await storage.getToken();
+            if (!user) setUser(user);
+      }
+
+      if (!isReady)
+            return (
+                  <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} onError={console.warn} />
+            );
 
       return (
             <AuthContext.Provider value={{ user, setUser }}>
